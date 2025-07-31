f242c68951be6558631edb23f1bd9ec394bb3386
imponenm <marcus.imponenti@gmail.com>
2025-07-30 23:09:14 -0700

    Done with tetris for now

--- recreations/tetris-theme.js
+++ recreations/tetris-theme.js
@@ -5,9 +5,9 @@ const introMelody = [
   "[a4, e3] [a4 c5] e5 [d5 c5]",
   "[b4 ,g#4] [[b4, g#4] [c5, a4]] [d5, b4] e5",
   "c5 a4 a4 ~",
-  "[~ d5@2 f5 a5@2 g5 f5]",
-  "[e5@3 c5 e5@2 d5 c5]",
-  "b4 [b4 c5] d5 e5",
+  "[~ d5@2 f5 a5@2 g5 f5, ~ f4@2 a4 c4@2 b4 c4]",
+  "[e5@3 c5 e5@2 d5 c5, g4@3 e4 g4@2 f4 g4]",
+  "b4 [b4 c5] d5 e5, g4 [g4 a4] b4 ~",
   "c5 a4 a4 ~"
 ];
 
@@ -28,13 +28,13 @@ const chorusMelody = introMelody.slice(1)
 const chorusBass = introBass.slice(1);
 
 const bridgeMelody = [
-  "e4@2 c4@2",
-  "d4@2 b3@2",
-  "c4@2 a3@2",
-  "g#3@2 b3@2",
-  "e4@2 c4@2",
-  "d4@2 b3@2",
-  "c4 e4 a4 a4",
+  "e4@2 c4@2, c4@2 a3@2",
+  "d4@2 b3@2, b3@2 g#3@2",
+  "c4@2 a3@2, a3@2 e3@2",
+  "g#3@2 b3@2, e3@2 g#3@2",
+  "e4@2 c4@2, c4@2 a3@2",
+  "d4@2 b3@2, b3@2 g#3@2",
+  "c4 e4 a4 a4, a3 c4 e4 e4",
   "<b4, e4, g#3>"
 ];
 
@@ -65,12 +65,16 @@ const fullMelody = cat(
   ...introMelody, 
   ...chorusMelody, 
   ...bridgeMelody,
-  ...chorusMelody
+  ...chorusMelody,
+  ...bridgeMelody,
+  ...chorusMelody,
 ).note();
 
 const fullBass = cat(
   ...introBass, 
   ...chorusBass, 
+  ...bridgeBass,
+  ...chorusBass,
   ...bridgeBass,
   ...chorusBass
 ).note();
